fb9086eef0188d998464b8fbde3537e4201e1795
William Mollenkopf <the.biri@gmail.com>
2015-06-01 22:59:53 -0400

    Updated the adapter to use a DB separate from the one being used for android Dev (to avoid conflicts)
    Also did some initial work for the takesurvey.php page so that it can pull in a question and display it with the appropriate answers on the page.

--- mysql_adapter.php
+++ mysql_adapter.php
@@ -2,18 +2,11 @@
 error_reporting(E_ERROR | E_PARSE); // hide php server level warning messages to users about db being offline etc
 
 $mysql_host = "mysql4.000webhost.com";
-$mysql_database = "a9634422_users";
-$mysql_user = "a9634422_stav";
+$mysql_database = "a9634422_survey";
+$mysql_user = "a9634422_survey";
 $mysql_password = "testpass1";
 
-	// Create connection
-	$con = mysqli_connect($mysql_host,$mysql_user,$mysql_password,$mysql_database);
-
-	// Check connection
-	if (mysqli_connect_errno())
-	{
-		echo "Failed to connect to Database";//: " . mysqli_connect_error();
-	}
-	
+	mysql_connect($mysql_host, $mysql_user,$mysql_password) or die(mysql_error());
+	mysql_select_db($mysql_database) or die(mysql_error());
 	//echo "Connected successfully";
 ?> 
--- takesurvey.php
+++ takesurvey.php
@@ -3,6 +3,46 @@
 $current_path = 'http://' . $_SERVER['HTTP_HOST'] . $_SERVER['DOCUMENT_ROOT'] . dirname($_SERVER['SCRIPT_NAME']);
 
  include('mysql_adapter.php'); ?>
+ 
+ 
+ <?php
+ //print_r($_GET);
+ $survey_id = $_GET["survey_id"];
+ if(!isset($survey_id)){die("Error.");} // fail the page if it doesn't have a survey_id
+ $question_id = -1; // preset to something not possible as a future lockout
+ 
+ if(isset($_GET["question_id"])){$question_id = $_GET["question_id"];}
+ else
+ {
+	$query0 = "select question_id from questions where survey_id = " . $survey_id . " order by question_id asc";
+	//print_r($query0);
+	$result0 = mysql_query($query0) or die("Database Error0: " . mysql_error()); 
+	$row0 = mysql_fetch_array($result0);
+	$question_id = $row0["question_id"];
+ }
+ 
+ // TODO: Add binding of php variable to avoid php injections
+ $query = "
+ select *
+ from questions
+ where survey_id = " . $survey_id . " and question_id = " . $question_id;
+ //print_r($query);
+ 
+ // Get the data
+ $result = mysql_query($query) or die("Database Error1: " . mysql_error()); 
+ $row = mysql_fetch_array($result);
+ 
+ // check if it's not a fill in the blank type question
+ if($row["question_id"]!=3) // that hardcoding tho...
+ {
+	$query2 = "select * from question_options where question_id = " . $question_id . " order by option_id asc";
+	$result2 = mysql_query($query2) or die("Database Error2: " . mysql_error()); 
+ }
+
+ 
+ // Need another query to populate previously answered questions
+ 
+ ?>
 <!DOCTYPE html>
 <html lang="en-US">
 
@@ -15,14 +55,33 @@ $current_path = 'http://' . $_SERVER['HTTP_HOST'] . $_SERVER['DOCUMENT_ROOT'] . 
 <body>
 
 	<form id="takeSurveyForm">
-	Question: <span id="questionNumber"></span> 
+	Question: <span id="questionNumber"><?php echo $row["question_id"]; ?></span> 
 	<!--Load question number above-->
 	<br>
 	
-	<div id="questionText"></div>
+	<div id="questionText"><?php echo $row["question_text"]; ?></div>
 	<!--Load question Text here-->
 	
-	<div id="questionAnswerChoices"></div>
+	<div id="questionAnswerChoices">
+	
+	<?php
+		if($row["question_id"]==3)
+		{
+			echo '<input type="text" id="answer" name="answer">';
+		}
+		else
+		{
+			echo "<table>";
+			while($row2 = mysql_fetch_array($result2)) 
+			{
+				echo '<tr>';
+				echo '<td><input type="radio" id="o_id' . $row2['option_id'] . '" value="' . $row2['option_id'] . '"> '. $row2['option_text'] .'</td>';
+				echo '</tr>';
+			}
+			echo "</table>";
+		}
+	?>
+	</div>
 	<!--Load question answer choices here-->
 	</form> 
 	
