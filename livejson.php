1493096104bc03d889e9a3886267b6a8e9224aac
Biri Komori <the.biri@gmail.com>
2015-06-14 00:42:41 -0400

    Updated the livejson to include some extra answer related json, and replaced "answer" json value with what was sometimes just the option_id (due to an improper database design on my part, it needs improvement but deadline is too soon to restructure :( )

--- livejson.php
+++ livejson.php
@@ -16,15 +16,26 @@ on users.user_id = answers.user_id
 INNER JOIN questions
 ON questions.question_id = answers.question_id
 AND questions.survey_id = answers.survey_id
+LEFT OUTER JOIN question_options
+ON question_options.question_id = answers.question_id
+AND question_options.survey_id = answers.survey_id
+AND question_options.option_id = answers.answer
 WHERE answers.survey_id = " . sanitize_value($survey_id) . "
 ORDER BY answers.ans_timestamp desc";
 //nice_print_r($query);
 // DO NOT USE run_query for this query!! the data this page echos must be perfect!
 $result = mysql_query($query) or die("Database Error: " . mysql_error()); 
 $jsonString = '{"data":[';
- 
+
 while($row = mysql_fetch_array($result)) 
 {
+	$answer = $row["answer"];
+	if($row["option_text"] != "" AND !is_null($row["option_text"]))
+	{
+		// we're dealing with a multiple choice question, use the option_text instead of the answer value (which is the option_id in this case)
+		$answer = $row["option_text"];
+		//nice_print_r($row);
+	}
 	$jsonString .= '{';
 	$jsonString .= '"question_id":"' . $row["question_id"] . '",';
 	$jsonString .= '"qtype_id":"' . $row["qtype_id"] . '",';
@@ -34,7 +45,10 @@ while($row = mysql_fetch_array($result))
 	$jsonString .= '"first_name":"' . $row["first_name"] . '",';
 	$jsonString .= '"last_name":"' . $row["last_name"] . '",';
 	$jsonString .= '"ans_timestamp":"' . $row["ans_timestamp"] . '",';
-	$jsonString .= '"answer":"' . $row["answer"] . '"';
+	$jsonString .= '"answer_db_value":"' . $row["answer"] . '",';
+	$jsonString .= '"option_id":"' . $row["option_id"] . '",';
+	$jsonString .= '"option_text":"' . $row["option_text"] . '",';
+	$jsonString .= '"answer":"' . $answer . '"';
 	$jsonString .= '},';
 }
 $jsonString = rtrim($jsonString, ","); // remove that last comma.
